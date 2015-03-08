1daf45371c7353a929fd1e45565eeea30aa53590
Grant Mathews <johnfn@gmail.com>
2015-03-07 16:32:55 -0800

    Ensure we have node webkit

--- TypeScriptHTMLApp1/nw-specific.js
+++ TypeScriptHTMLApp1/nw-specific.js
@@ -1,49 +1,51 @@
 // Load native UI library
-var gui = require('nw.gui'); //or global.window.nwDispatcher.requireNwGui() (see https://github.com/rogerwang/node-webkit/issues/707)
+if (typeof require !== 'undefined') {
+    var gui = require('nw.gui');
 
-var win = gui.Window.get();
-var tray;
+    var win = gui.Window.get();
+    var tray;
 
-win.menu = new gui.Menu({ type: 'menubar' });
-win.menu.createMacBuiltin("grants-todos");
+    win.menu = new gui.Menu({ type: 'menubar' });
+    win.menu.createMacBuiltin("grants-todos");
 
 // Get the minimize event
-win.on('minimize', function() {
-  // Hide window
-  win.hide();
+    win.on('minimize', function() {
+        // Hide window
+        win.hide();
 
-  // Show tray
-  tray = new gui.Tray({ icon: 'icon.png' });
+        // Show tray
+        tray = new gui.Tray({ icon: 'icon.png' });
 
-  // Show window and remove tray when clicked
-  tray.on('click', function() {
-    win.show();
-    tray.remove();
-    tray = null;
-  });
-});
+        // Show window and remove tray when clicked
+        tray.on('click', function() {
+            win.show();
+            tray.remove();
+            tray = null;
+        });
+    });
 
-win.on('restore', function() {
-  if (tray) {
-    tray.remove();
-    tray = null;
-  }
+    win.on('restore', function() {
+        if (tray) {
+            tray.remove();
+            tray = null;
+        }
 
-  win.focus()
-});
+        win.focus()
+    });
 
-var option = {
-  key : "Ctrl+Period",
-  active : function() {
-    win.restore();
+    var option = {
+        key: "Ctrl+Period",
+        active: function() {
+            win.restore();
+            win.focus();
+        },
+        failed: function(msg) {
+            console.log(msg);
+        }
+    };
+
+    var shortcut = new gui.Shortcut(option);
+    gui.App.registerGlobalHotKey(shortcut);
+
     win.focus();
-  },
-  failed : function(msg) {
-    console.log(msg);
-  }
-};
-
-var shortcut = new gui.Shortcut(option);
-gui.App.registerGlobalHotKey(shortcut);
-
-win.focus();
+}
